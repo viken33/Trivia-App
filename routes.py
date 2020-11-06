865f53fc28b247a0cc6327aec8138562c124f3f8
viken33 <viken33@gmail.com>
2020-11-05 23:27:14 -0300

    sessions and bootstrap

--- routes.py
+++ routes.py
@@ -2,23 +2,25 @@
 from apptrivia import app
 import random
 import datetime
+import functools
+
 
 # importamos los modelos a usar
 from models.models import Categoria, Pregunta, Respuesta
 
-from flask import render_template, session
+from flask import render_template, session, redirect, url_for
 
 @app.route('/trivia')
 def index():
     session.clear()
+    session["inicio"] = datetime.datetime.now()
     return render_template('home.html')
-    session[inicio] = datetime.datetime.now()
-
+    
 
 @app.route('/trivia/categorias', methods=['GET'])
 def mostrarcategorias():
     categorias = Categoria.query.all()
-    return render_template('categorias.html', categorias=categorias)
+    return render_template('categorias.html', categorias=categorias, session=session)
 
 
 @app.route('/trivia/<int:id_categoria>/pregunta', methods=['GET'])
@@ -35,13 +37,19 @@ def mostrarpregunta(id_categoria):
 def mostrarrespuesta(id_respuesta, id_pregunta):
     pregunta = Pregunta.query.get(id_pregunta)
     respuesta = Respuesta.query.get(id_respuesta)
+    categorias = Categoria.query.all()
     respuesta.text = "mmmmnnnnaaaaaaaaaa"
     if respuesta.correcta:
-        #session[pregunta.categoria_id] = True
+        session[str(pregunta.categoria_id)] = True
         respuesta.text = "COOOORRRECTOOOO"
+        completado = [x for x in categorias if str(x.id) in session]
+        if len(completado) == len(categorias):
+            return redirect(url_for('ganaste'))
+        
     return render_template('respuesta.html', pregunta=pregunta, respuesta=respuesta)
 
 
 @app.route('/trivia/ganaste', methods=['GET'])
 def ganaste():
-    return render_template('ganaste.html')
+    duracion = datetime.datetime.now()-session["inicio"]
+    return render_template('ganaste.html', duracion=duracion)
